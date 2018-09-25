f402ad810f10cb027bb05ab3591e8df4be7382c2
zhaohuan-vip <gzhaohuan@gmail.com>
2018-09-25 11:48:37 +0800

    add images

---
+++ settings.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# -*- Author: zhaohuan -*-
+
+
+class Settings():
+    """存储《外星人入侵》的所有设置的类"""
+
+    def __init__(self):
+        """初始化游戏的设置"""
+        # 屏幕设置
+        self.screen_width = 800
+        self.screen_height = 600
+        self.bg_color = (230, 230, 230)
