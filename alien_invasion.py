5817337d8aa835b92d5ab3b3eee3e8696663f3f9
zhaohuan-vip <gzhaohuan@gmail.com>
2018-09-25 18:10:05 +0800

    12.8.5

--- alien_invasion.py
+++ alien_invasion.py
@@ -27,6 +27,11 @@ def run_game():
         gf.check_events(ai_settings,screen,ship,bullets)
         ship.update()
         bullets.update()
+
+        for bullet in bullets:
+            if bullet.rect.bottom <=0:
+                bullets.remove(bullet)
+        print(len(bullets))
         gf.update_screen(ai_settings,screen,ship,bullets)
 
 run_game()
